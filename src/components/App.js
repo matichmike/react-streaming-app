f07898b6609ae44f20c72c89a8d578781ba33d93
Michael Matich <matich.mike@gmail.com>
2020-09-04 18:17:06 -0400

    navigantion boiler plate

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
-const PageOne = () => {
-  return <div>
-    PageOne
-    <Link to="/two">To page 2</Link>
-  </div>
-}
-
-const PageTwo = () => {
-  return <div>
-    <button>PageTwo</button>
-    <Link to="/">To page 1</Link>
-    </div>
-}
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
       <div>
-        <Route path="/" exact component={PageOne} />
-        <Route path="/two" component={PageTwo} />
+      <Header />
+       <Route path="/" exact component={StreamList} />
+       <Route path="/streams/new" exact component={StreamCreate} />
+       <Route path="/streams/edit" exact component={StreamEdit} />
+       <Route path="/streams/delete" exact component={StreamDelete} />
+       <Route path="/streams/show" exact component={StreamShow} />
       </div>
       </BrowserRouter>
     </div>
